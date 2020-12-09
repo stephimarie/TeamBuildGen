fb41bd0aa60167afe3eb8ad184f1e1ea1a1e2125
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-08 23:59:10 -0800

    Add Intern test

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -1,2 +1,7 @@
 const Intern = require("../lib/Intern");
 
+test("Can set school via constructor", () =>{
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
