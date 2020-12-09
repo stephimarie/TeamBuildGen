8c838fc5a22ec905c50ac6e5fb2714f672f6a35f
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-08 23:48:11 -0800

    Add Engineer test

--- Develop/test/Engineer.test.js
+++ Develop/test/Engineer.test.js
@@ -1,1 +1,8 @@
 const Engineer = require("../lib/Engineer");
+
+test("Can set GitHub account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer("Foo",1, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
+
