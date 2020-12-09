dbf4fd266eb5c3d188006cb507a6ed8570ca0840
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-08 23:38:37 -0800

    Add Employee test

--- Develop/test/Employee.test.js
+++ Develop/test/Employee.test.js
@@ -35,7 +35,11 @@ test("Can get id via getId()", () => {
     expect(e.getId()).toBe(testValue);
 });
 
-
+test("Can get email via getEmail()", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Foo", 1, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
 
 
 
