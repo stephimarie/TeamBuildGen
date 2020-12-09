fcf42fb06aa1b1485152b40d5bcec6a22bc6e00e
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-08 23:22:22 -0800

    Add Employee test

--- Develop/test/Employee.test.js
+++ Develop/test/Employee.test.js
@@ -4,3 +4,10 @@ test("Can instantiate Employee instance", () => {
     const e = new Employee();
     expect(typeof(e)).toBe("object");
 });
+
+test("Can set name via constructor arguments", () => {
+    const name = "Stephanie";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
