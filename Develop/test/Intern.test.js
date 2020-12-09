99529b5f3be2d9f532b741e3edbef9bea68052ee
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-09 00:04:27 -0800

    Add Intern test

--- Develop/test/Intern.test.js
+++ Develop/test/Intern.test.js
@@ -11,3 +11,9 @@ test("getRole() should return \"Intern\"", () => {
     const e = new Intern("Foo", 1, "test@test.com", "UCLA");
     expect(e.getRole()).toBe(testValue);
 });
+
+test("Can get school via getSchool()", () => {
+    const testValue = "UCLA";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
