fe83136cbaa835644f07346cdff01da447942657
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-08 23:42:53 -0800

    Add Employee test

--- Develop/test/Employee.test.js
+++ Develop/test/Employee.test.js
@@ -41,7 +41,11 @@ test("Can get email via getEmail()", () => {
     expect(e.getEmail()).toBe(testValue);
 });
 
-
+test("getRole() should return \"Employee\"", () => {
+    const testValue = "Employee";
+    const e = new Employee("Stephanie", 1, "test@test.com");
+    expect(e.getRole()).toBe(testValue);
+});
 
 
 
