be9ebd78ca379513f12d5f0ef9bd78a61ead7c95
Stephanie McCandless <mccandlessstephi@gmail.com>
2020-12-10 22:26:20 -0800

    add const rendermain

--- Develop/lib/htmlRenderer.js
+++ Develop/lib/htmlRenderer.js
@@ -52,3 +52,8 @@ const renderEngineer = engineer => {
     template = replacePlaceholders(template, "school", intern.getSchool());
     return template;
   };
+
+  const renderMain = html => {
+    const template = fs.readFileSync(path.resolve(templatesDir, "main.html"), "utf8");
+    return replacePlaceholders(template, "team", html);
+  };
