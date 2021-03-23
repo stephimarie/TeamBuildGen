63b0416e43156a8fa757690602a54471a1e19b2e
Stephanie McCandless <mccandlessstephi@gmail.com>
2021-03-22 23:07:50 -0700

    added string

--- app.js
+++ app.js
@@ -40,3 +40,14 @@ const isNumeric = async (input) => {
     return true;
   };
   
+  // This formats the input given by the user
+  const formatTitleCase = async (string) => {
+    return string
+      .split(" ")
+      .map((word) => {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+      })
+      .join(" ")
+      .trim();
+  };
+  
